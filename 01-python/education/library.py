c2f9a02656fe3292752ae31deb2e0b293655a1f3
David Wang <djwang826@gmail.com>
2020-09-12 15:05:11 -0400

    added variable amounts of books to the library, set default value to 3

--- 01-python/education/library.py
+++ 01-python/education/library.py
@@ -1,3 +1,9 @@
-def draw_library():
-    print("library not found")
+def draw_library(numBook = 3):
+    for i in range(numBook):
+        print("                   _______   \n\
+                  /      /,  \n\
+                 /      //   \n\
+                /______//    \n\
+               (______(/    \n\
+            ")
     return
