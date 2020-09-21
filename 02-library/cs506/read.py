aae4b41814214623bca40376645684af11d7ea84
David Wang <djwang826@gmail.com>
2020-09-21 15:42:03 -0400

    Finished exercise 2

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,22 @@
+from csv import reader
+
 def read_csv(csv_file_path):
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    res = []
+
+    readFile = reader(open(csv_file_path))
+
+    #Check if there is number if yes change to integer else keep as string
+    for row in readFile:
+        list_row = []
+        for val in row:
+            if val.isdigit():
+                list_row.append(int(val))
+            else:
+                list_row.append(val)
+        res.append(list_row)
+    
+    return res
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,52 @@
+import math
+import decimal
+
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    return (p_root(sum(pow(abs(a - b), 2)for a, b in zip(x, y)), 2))
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return (p_root(sum(pow(abs(a - b), 1)for a, b in zip(x, y)), 1))
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    if not x and not y:
+        return 1
+    return 1 - jaccard_sim(x,y)
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    if (len(x) != len(y)):
+        return 'vectors not same size'
+    
+    dotProd = dot(x, y)
+    normX = mag(x)
+    normY = mag(y)
 
+    if (normX * normY) != 0:
+        return dotProd / (normX * normY)
+    else:
+        return float('inf')
+    
 # Feel free to add more
+#Root value function for Minkowski distance
+def p_root(val, r):
+    root = 1 / float(r)
+    return round(decimal.Decimal(val) ** decimal.Decimal(root), 3)
+
+#Jaccard Similarity function
+def jaccard_sim(x, y):
+    s1 = set(x)
+    s2 = set(y)
+    return len(s1.intersection(s2)) / len(s1.union(s2))
+
+#magnitude func in place of np.linalg.norm
+def mag(x):
+    res = 0
+
+    for i in x:
+        res += i **2
+    
+    return math.sqrt(res)
+
+#dot product function
+def dot(x, y):
+    return sum([i*j for (i, j) in zip(x, y)])
